7118256818d826e98182e023d1e421c95e40af78
Shaurya Gupta <vishnu8858369983@gmail.com>
2025-04-24 16:47:49 +0530

    all good till now

--- src/routes/user.js
+++ src/routes/user.js
@@ -92,6 +92,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
 
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    let skip = (page - 1) * 10
+
     //find all connection request which are sent + received from/to loggedInUser respectively
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -118,9 +122,9 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUserFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit)
 
-    res.send(userExceptHidden);
+    res.json({data: userExceptHidden});
 
     // console.log(userExceptHidden);
   } catch (err) {
